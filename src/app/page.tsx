c540d135b72104d5d87cd59fdd87dca2f87e7000
Lxdovic <ludovicdebever0@gmail.com>
2024-01-29 11:20:24 +0100

    feat(hover-card): Added hover-card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Sphere from '@/components/r3f/sphere'
 import {Navbar} from '@/features/navbar'
+import {ThreeDCardDemo} from '@/features/hover-card'
 
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex flex-col">
       <Navbar />
 
-      <div className="flex h-screen w-screen">
+      <div className="flex h-screen">
         <div className="w-1/5 shrink-0"></div>
         <div className="flex w-4/5 border-l">
           <div
@@ -26,8 +27,13 @@ export default function Home() {
               <span>Ludovic Debever</span>
             </h1>
           </div>
+
           <Sphere />
         </div>
+      </div>
+
+      <div className="flex h-screen justify-center">
+        <ThreeDCardDemo />
       </div>
     </main>
   )
