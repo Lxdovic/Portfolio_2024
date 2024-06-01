94cd75425b626352e90b26dea829b17eb1888b13
ludovic <ludovicdebever0@gmail.com>
2024-06-01 11:16:47 +0200

    style(challenge): made article responsive

--- src/app/projects/chess-challenge/page.tsx
+++ src/app/projects/chess-challenge/page.tsx
@@ -3,7 +3,7 @@ import ChessChallengeArticle from '@/app/projects/chess-challenge/article'
 export default function Page() {
   return (
     <section className="mt-40 flex w-full justify-center">
-      <div className="flex max-w-2xl flex-col gap-6 py-10">
+      <div className="flex sm:max-w-lg max-w-xs md:max-w-2xl flex-col gap-6 py-10">
         <ChessChallengeArticle />
       </div>
     </section>
