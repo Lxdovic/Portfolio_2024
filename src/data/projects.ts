a99075e6f0e47e3b91469a6510000d326bb3e5a3
ludovic <ludovicdebever0@gmail.com>
2024-06-01 11:16:23 +0200

    feat(projects): added rhino and zoidberg

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,56 +9,49 @@ export type Project = {
 import abstract1 from '@/assets/images/abstract1.jpg'
 import chessChallenge from '@/assets/images/chess-challenge/chess-challenge.png'
 import chessEngine from '@/assets/images/chess1.png'
-import ircApp from '@/assets/images/abstract2.jpg'
-import pathfinder from '@/assets/images/abstract3.jpg'
+import abstract2 from '@/assets/images/abstract2.jpg'
+import abstract3 from '@/assets/images/abstract3.jpg'
 import portfolio from '@/assets/images/abstract4.jpg'
 
 export const projects: Project[] = [
   {
     title: 'Chess Challenge',
     href: '/projects/chess-challenge',
-    image: chessChallenge,
+    image: abstract1,
     alt: 'Abstract blue and pink background',
     description:
       'My submission to a coding challenge hosted by Sebastian Lague. I was given a chess framework and had to implement a chess bot on top of it. My bot ended top 63 out of 600+ submissions.',
   },
   {
     title: 'Chess Engine',
-    href: '/projects/chess-engine',
+    href: 'https://github.com/Lxdovic/Lxna',
     image: chessEngine,
     alt: 'Chess board with pieces on it',
     description:
       'A chess engine I built from scratch. Utilizing many chess engine techniques such as bitboards, magic bitboards, negamax, alpha-beta pruning, quiescence search, transposition tables, null move pruning, and more.',
   },
   {
-    title: 'IRC App',
-    href: '/projects/irc-app',
-    image: ircApp,
+    title: 'Rhino',
+    href: 'https://github.com/Lxdovic/Rhino',
+    image: abstract2,
     alt: 'Abstract purple and pink background',
     description:
-      'An Internet Relay Chat (IRC) app built using websockets. It has features like private messaging channels, user authentication, guests, images and gifs, emojis and more.',
+      'A programming language I built from scratch. It features a REPL, a lexer, a parser, an interpreter and a compiler.',
   },
   {
-    title: 'Pathfinder',
-    href: '/projects/pathfinder',
-    image: pathfinder,
+    title: 'Neural Network',
+    href: 'https://github.com/Lxdovic/Epitech-Zoidberg',
+    image: abstract3,
     alt: 'Abstract blue and pink background',
     description:
-      'My implementation of the A* algorithm along with a Three.js scene for visualization.',
+      'A neural network I built from scratch. It is able to detect pneumonia in X-ray images with an accuracy of ~94%.',
   },
   {
     title: 'This website',
-    href: '/projects/portfolio',
+    href: 'https://github.com/Lxdovic/Portfolio_2024',
     image: portfolio,
     alt: 'Abstract black, pink, blue, yellow and purple background',
     description:
       'My personal website, built with Next.js, TypeScript, and Tailwind CSS.',
-  },
-  {
-    title: 'More',
-    href: 'https://github.com/Lxdovic',
-    image: abstract1,
-    alt: 'Abstract blue and pink background',
-    description: 'more of my projects can be found on my GitHub profile.',
   },
 ]
