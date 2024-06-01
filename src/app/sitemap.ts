edd7da0390166726b2e7d1071db3eec02ae3ad02
ludovic <ludovicdebever0@gmail.com>
2024-06-01 11:38:08 +0200

    build: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://lxdovic.fr',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://lxdovic.fr/projects/chess-challenge',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+  ]
+}
