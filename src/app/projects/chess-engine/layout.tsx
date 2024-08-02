bddc042aaaf4e13fa55fbc5222d4c7fbe54d82bd
Lxdovic <ludovicdebever0@gmail.com>
2024-08-02 13:44:04 +0200

    fix(navbar): fix navbar position

--- src/app/projects/chess-engine/layout.tsx
+++ src/app/projects/chess-engine/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <main>
+    <main className="flex flex-col">
       <Navbar />
       {children}
     </main>
